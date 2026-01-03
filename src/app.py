e78b76541db609d0a26ea5f85e7e5e9ca69159b1
Ckal <christof.kaller@gmail.com>
2026-01-03 08:55:06 +0100

    test: trigger CI with all fixes applied (v1.0.2)

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from transformers.tools.base import launch_gradio_demo
 from emojify_text import EmojifyTextTool
 
-# Test deployment - v1.0.1
+# Test deployment - v1.0.2
 launch_gradio_demo(EmojifyTextTool)
